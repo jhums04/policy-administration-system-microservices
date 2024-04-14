35aed3626bd4ce416abbb51912eec931e3b7e231
jhums04 <jbonganciso04@gmail.com>
2024-04-14 08:38:56 +0800

    modify account controller and service

--- src/main/java/com/policyadminsystem/policyadminsystem/controller/CustomerAccountOwnerController.java
+++ src/main/java/com/policyadminsystem/policyadminsystem/controller/CustomerAccountOwnerController.java
@@ -2,15 +2,17 @@ package com.policyadminsystem.policyadminsystem.controller;
 
 import com.policyadminsystem.policyadminsystem.dto.AccountOwnerDTO;
 import com.policyadminsystem.policyadminsystem.service.AccountOwnerService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/account-owner")
-@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/account-owner")
 public class CustomerAccountOwnerController {
 
+    @Autowired
     private AccountOwnerService accountOwnerService;
 
 
